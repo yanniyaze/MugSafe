847fafcac2e3c1fbd49be9ee3344d4b6771392d7
Yannik Lehnhardt <yannik_lehnhardt@gmx.de>
2026-04-28 14:32:43 +0200

    Add ino-File and library structure :D

---
+++ MugSafe.ino
@@ -0,0 +1,13 @@
+#include "libraries/led/led_abwechselnd.cpp"
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  led_abwechselnd();
+}
---
+++ libraries/led/led_abwechselnd.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+const int BLUE = 12;
+const int GREEN = 10;
+const int RED = 8;
+
+void led_abwechselnd() {
+  int state = RED;
+    while (true) {
+      switch (state) {
+        case RED:
+          digitalWrite(RED, LOW);
+          digitalWrite(GREEN, HIGH);
+          delay(500);
+          state = GREEN;
+
+          break;
+        case BLUE:
+          digitalWrite(BLUE, LOW);
+          digitalWrite(RED, HIGH);
+          delay(500);
+          state = RED;
+          break;
+        case GREEN:
+          digitalWrite(GREEN, LOW);
+          digitalWrite(BLUE, HIGH);
+          delay(500);
+          state = BLUE;
+          break;
+    }
+  }
+}
