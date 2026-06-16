de9edf4985c8e20e85aa7e7a2eed89b4018eb74b
Yannik Lehnhardt <yannik_lehnhardt@gmx.de>
2026-06-16 07:31:05 +0200

    Add servo idea

--- sandbox/sandbox.ino
+++ sandbox/sandbox.ino
@@ -1,34 +1,33 @@
-#include "libraries/piezo/buzzer.cpp"
+#include <Servo.h>
 
-#define UP_BUTTON_PIN 6
-#define DOWN_BUTTON_PIN 4
-#define CONFIRM_BUTTON_PIN 2
-#define PIEZO 11
+Servo myservo;
+
+#define DEFAULT_PULSE_WIDTH 0;
 
 void setup() {
-  pinMode(PIEZO, OUTPUT);
-  pinMode(UP_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(CONFIRM_BUTTON_PIN, INPUT_PULLUP);
+  delay(1000);
+  myservo.attach(9); 
+  myservo.write(90);    // Stop/Neutral zuerst setze
 }
 
 void loop() {
-  byte upButtonState = digitalRead(UP_BUTTON_PIN);
-  byte downButtonState = digitalRead(DOWN_BUTTON_PIN);
-  byte confirmButtonState = digitalRead(CONFIRM_BUTTON_PIN);
+  moveUp();
 
+  delay(2000);
 
-  if (upButtonState == LOW) {
-    playPositive(PIEZO);
-  }
+  //moveDown();
+  myservo.detach();
+  while (1) {}
+}
 
-  if (downButtonState == LOW) {
-    playNegative(PIEZO);
-  }
+void moveUp() {
+  myservo.write(180);
+  delay(250);
+  myservo.write(90);
+}
 
-  if (confirmButtonState == LOW) {
-    playConfirm(PIEZO);
-  }
-
-  
+void moveDown() {
+  myservo.write(0);
+  delay(250);
+  myservo.write(90);
 }
